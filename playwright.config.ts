195209630870b4180f40fe815807fd057aafc5ee
LuisSmada <148660352+LuisSmada@users.noreply.github.com>
2026-01-21 23:29:02 +0100

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,11 +4,10 @@ import { defineBddConfig } from "playwright-bdd";
 const testDir = defineBddConfig({
   features: "tests/features/**/*.feature",
   steps: "tests/steps/**/*.ts",
-  // output par défaut : .feature-gen (chez toi)
 });
 
 export default defineConfig({
-  testDir: testDir, // ✅ ici c'est une string (un dossier)
+  testDir: testDir, 
   use: {
     baseURL: "https://www.saucedemo.com",
     headless: true,
